08ea2b729a7b22d925194f78389360205bfb13ee
Dany Mrushi <mrushidany@gmail.com>
2025-08-04 15:01:26 +0300

    Customize the sign in page and removed the google and github signin applications

--- src/components/auth/SignIn/SignIn.tsx
+++ src/components/auth/SignIn/SignIn.tsx
@@ -3,7 +3,6 @@
 import Logo from '@/components/template/Logo'
 import Alert from '@/components/ui/Alert'
 import SignInForm from './SignInForm'
-import OauthSignIn from './OauthSignIn'
 import ActionLink from '@/components/shared/ActionLink'
 import useTimeOutMessage from '@/utils/hooks/useTimeOutMessage'
 import useTheme from '@/utils/hooks/useTheme'
@@ -18,10 +17,8 @@ type SignInProps = {
 }
 
 const SignIn = ({
-    signUpUrl = '/sign-up',
     forgetPasswordUrl = '/forgot-password',
     onSignIn,
-    onOauthSignIn,
 }: SignInProps) => {
     const [message, setMessage] = useTimeOutMessage()
 
@@ -29,32 +26,32 @@ const SignIn = ({
 
     return (
         <>
-            <div className="mb-8">
+            <div className='mb-8'>
                 <Logo
-                    type="streamline"
+                    type='streamline'
                     mode={mode}
                     logoWidth={60}
                     logoHeight={60}
                 />
             </div>
-            <div className="mb-10">
-                <h2 className="mb-2">Welcome back!</h2>
-                <p className="font-semibold heading-text">
+            <div className='mb-10'>
+                <h2 className='mb-2'>Welcome back!</h2>
+                <p className='font-semibold heading-text'>
                     Please enter your credentials to sign in!
                 </p>
             </div>
             {message && (
-                <Alert showIcon className="mb-4" type="danger">
-                    <span className="break-all">{message}</span>
+                <Alert showIcon className='mb-4' type='danger'>
+                    <span className='break-all'>{message}</span>
                 </Alert>
             )}
             <SignInForm
                 setMessage={setMessage}
                 passwordHint={
-                    <div className="mb-7 mt-2">
+                    <div className='mb-7 mt-2'>
                         <ActionLink
                             href={forgetPasswordUrl}
-                            className="font-semibold heading-text mt-2 underline"
+                            className='font-semibold heading-text mt-2 underline'
                             themeColor={false}
                         >
                             Forgot password
@@ -63,31 +60,6 @@ const SignIn = ({
                 }
                 onSignIn={onSignIn}
             />
-            <div className="mt-8">
-                <div className="flex items-center gap-2 mb-6">
-                    <div className="border-t border-gray-200 dark:border-gray-800 flex-1 mt-[1px]" />
-                    <p className="font-semibold heading-text">
-                        or countinue with
-                    </p>
-                    <div className="border-t border-gray-200 dark:border-gray-800 flex-1 mt-[1px]" />
-                </div>
-                <OauthSignIn
-                    setMessage={setMessage}
-                    onOauthSignIn={onOauthSignIn}
-                />
-            </div>
-            <div>
-                <div className="mt-6 text-center">
-                    <span>{`Don't have an account yet?`} </span>
-                    <ActionLink
-                        href={signUpUrl}
-                        className="heading-text font-bold"
-                        themeColor={false}
-                    >
-                        Sign up
-                    </ActionLink>
-                </div>
-            </div>
         </>
     )
 }
