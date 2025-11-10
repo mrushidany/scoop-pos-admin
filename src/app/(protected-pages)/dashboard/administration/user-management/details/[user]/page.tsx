8678d6a972044d9cba2f166571173e90b94c0ab1
Dany Mrushi <mrushidany@gmail.com>
2025-11-10 14:06:58 +0300

    Set up the user details route

--- src/app/(protected-pages)/dashboard/administration/user-management/details/[user]/page.tsx
+++ src/app/(protected-pages)/dashboard/administration/user-management/details/[user]/page.tsx
@@ -1,12 +1,37 @@
 'use client'
 
 import React, { use } from 'react'
+import { useRetrieveUserDetails } from '@/hooks/features/user-management/userManagementApi'
+import NoUserFound from '@/assets/svg/NoUserFound'
+import { getApiErrorMessage } from '@/utils/apiError'
+import Loading from '@/components/shared/Loading'
 
 export default function Page({ params }: { params: Promise<{user: number}> }) {
     const resolvedParams = use(params)
     const { user }  = resolvedParams
 
-    console.log('Who is this user here : ', user)
+    const { data, isLoading, error } = useRetrieveUserDetails(user)
+
+    if(error || !data) {
+        return (
+            <div className='h-full flex flex-col items-center justify-center'>
+                <NoUserFound height={280} width={280} />
+                <h2 className='mt-4'>{Boolean(error) ? 'Error loading user!' : 'No user found!'}</h2>
+                {Boolean(error) && (
+                    <p className='text-red-500 mt-2'>
+                        {getApiErrorMessage(error)}
+                    </p>
+                )}
+            </div>
+
+        )
+    }
+
+    if (isLoading) {
+       return <Loading type='default' loading={isLoading} />
+    }
+
+    console.log('What are the user details here : ', data)
 
     return (
         <div>
--- src/configs/routes.config/administrationRoute.ts
+++ src/configs/routes.config/administrationRoute.ts
@@ -21,6 +21,14 @@ const administrationRoute: Routes = {
             pageContainerType: 'contained',
         },
     },
+    '/dashboard/administration/user-management/details/[user]': {
+        key: 'administration.users.userManagement',
+        authority: [ADMIN, USER],
+        dynamicRoute: true,
+        meta: {
+            pageContainerType: 'contained',
+        },
+    },
 }
 
 export default administrationRoute
