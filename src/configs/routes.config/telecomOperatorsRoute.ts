2ddf081beaffd1ae8a16d66915ed26795384b72f
Dany Mrushi <mrushidany@gmail.com>
2025-11-13 15:52:43 +0300

    Updated the route for telecom operators

--- src/configs/routes.config/telecomOperatorsRoute.ts
+++ src/configs/routes.config/telecomOperatorsRoute.ts
@@ -9,7 +9,35 @@ const telecomOperatorsRoute: Routes = {
             pageBackgroundType: 'plain',
             pageContainerType: 'contained',
         },
-    }
+    },
+    
+    '/dashboard/telecom-operators/create': {
+        key: 'telecomOperators.telecomOperators',
+        authority: [],
+        dynamicRoute: true,
+        meta: {
+            header: {
+                title: 'Create telecom operator'
+            },
+            pageContainerType: 'contained',
+        },
+    },
+    '/dashboard/telecom-operators/details/[telecomOperator]': {
+        key: 'telecomOperators.telecomOperators',
+        authority: [],
+        dynamicRoute: true,
+        meta: {
+            pageContainerType: 'contained',
+        },
+    },
+    '/dashboard/telecom-operators/edit/[telecomOperator]': {
+        key: 'telecomOperators.telecomOperators',
+        authority: [],
+        dynamicRoute: true,
+        meta: {
+            pageContainerType: 'contained',
+        },
+    },
 }
 
 export default telecomOperatorsRoute
