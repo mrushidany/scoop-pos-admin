362be4bf85b6470f11b0d48fa80c04cfa364ad8c
Dany Mrushi <mrushidany@gmail.com>
2025-12-02 11:57:32 +0300

    Leave the overview section empty

--- src/app/(protected-pages)/dashboard/overview/page.tsx
+++ src/app/(protected-pages)/dashboard/overview/page.tsx
@@ -1,18 +1,18 @@
-import Overview from './_components/Overview'
-import RecentOrder from './_components/RecentOrder'
-import getEcommerceDashboard from '@/server/actions/getEcommerceDashboard'
+// import Overview from './_components/Overview'
+// import RecentOrder from './_components/RecentOrder'
+// import getEcommerceDashboard from '@/server/actions/getEcommerceDashboard'
 
 export default async function Page() {
-    const data = await getEcommerceDashboard()
+    // const data = await getEcommerceDashboard()
     return (
         <div>
             <div className='flex flex-col gap-4 max-w-full overflow-x-hidden'>
                 <div className='flex flex-col xl:flex-row gap-4'>
                     <div className='flex flex-col gap-4 flex-1 xl:col-span-3'>
-                        <Overview data={data.statisticData} />
+                        {/* <Overview data={data.statisticData} /> */}
                     </div>
                 </div>
-                <RecentOrder data={data.recentOrders} />
+                {/* <RecentOrder data={data.recentOrders} /> */}
             </div>
         </div>
     )
